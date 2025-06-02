76d2552b3b2cc2c5830e4526aa4fedd2af18275e
wakeisle9933 <wakeisle9933@gmail.com>
2025-06-02 16:14:26 +0900

    Initial Commit

---
+++ src/main/java/com/art/main/kpmonitor/KpMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.art.main.kpmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/art/main/kpmonitor/KpMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art.main.kpmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
